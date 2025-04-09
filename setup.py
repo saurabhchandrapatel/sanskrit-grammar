621ec2a08e0da3686ca537113d8754576f887047
Saurabh Patel <vsaurabh.aec@gmail.com>
2025-04-09 22:40:41 +0530

    module update and bug fixes

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name='sanskrit-grammar',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[],  # list any dependencies here
     author='Saurabh Chandra Patel',
     author_email='vsaurabhaec@gmail.com',
-    description='Python module provides functions to handle and manipulate various aspects of Sanskrit grammar, including word generation, prefixes (Upasarga), suffixes (Pratyaya), verb conjugation, noun declension, Sandhi (joining words), Sandhi Vechheda (splitting words), and tense identification',
-    long_description=open('README.md').read(),
+    description='Python module provides functions to handle and manipulate various aspects of Sanskrit grammar, including word generation, prefixes etc',
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/saurabhchandrapatel/sanskrit-grammar',
     classifiers=[
